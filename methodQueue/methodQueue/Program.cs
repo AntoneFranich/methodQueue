7e86c708080f568a65e1aeabf3f41b88c635f093
AntoneFranich <30742849+AntoneFranich@users.noreply.github.com>
2017-10-24 21:13:15 -0700

    Initial commit

---
+++ methodQueue/methodQueue/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace methodQueues_CodeSample
+{
+	class Program
+	{
+		//The singular methodQueue dictionary. Call by calling the referencing Thread.CurrentThread.
+		public static Dictionary<Thread, MethodQueue> methodQueues = new Dictionary<Thread, MethodQueue>();
+		private static Thread mainThread; //The Thread Main is called from.
+		static void Main(string[] args)
+		{
+			mainThread = Thread.CurrentThread;
+			//Add a methodQueue for the mainThread
+			methodQueues.Add(mainThread, new MethodQueue(mainThread));
+
+			//Start the Created Thread.
+			StartThread();
+
+			//Wait 2 seconds, then trigger check on mainThread's methodQueue clear.
+			TriggerUpdate(2000);//Normally this would just be called as part of a primary loop. (IE: a Timer in WPF, or Update() in Unity)
+
+			Console.ReadKey();
+		}
+
+		//Simple thread that just waits x milliseconds, then calls the function to clear this thread's method Queue.
+		private static void TriggerUpdate(int wait)
+		{
+			Thread.Sleep(wait);
+			methodQueues[Thread.CurrentThread].Tick();
+		}
+
+		//A simple check that determines if the thread calling this is the mainThread or the thread we creatd at runtime.
+		private static void IsMainThread()
+		{
+			if (Thread.CurrentThread != mainThread)
+			{
+				Console.WriteLine("Executing From Created Thread:" + Thread.CurrentThread.ManagedThreadId);
+			}
+			else
+			{
+				Console.WriteLine("Executing From Main:" + +Thread.CurrentThread.ManagedThreadId);
+			}
+		}
+
+		//Starts a new thread.
+		private static void StartThread()
+		{
+			Thread newThread = new Thread(new ThreadStart(() => DoThreadWork()));
+			newThread.IsBackground = true;
+			newThread.Start();
+		}
+
+		//Runs at the start of the created thread.
+		private static void DoThreadWork()
+		{
+			//Create a methodQueue for this thread.
+			methodQueues.Add(Thread.CurrentThread, new MethodQueue(Thread.CurrentThread));
+
+			//Setup the method to be called when passed to the mainThread.
+			Action mainThreadMethod = () => IsMainThread();
+
+			//Setup the method to be called back to this thread once execution on the main thread is complete.
+			Action thisThreadCallback = () => IsMainThread();
+
+			//Setup the remote call on the mainThread.
+			methodQueues[mainThread].AddMethod(mainThreadMethod, Thread.CurrentThread, thisThreadCallback);
+
+			//Wait 4 seconds, then check to see if the callback we defined a couple lines above is ready to be called on the created thread.
+			TriggerUpdate(4000);
+		}
+	}
+}
---
+++ methodQueue/methodQueue/methodQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace methodQueues_CodeSample
+{
+	public class MethodQueue
+	{
+		//A custom struct holding information on the delegate method to call, the thread that call came from, and the delegate callback method to be called after execution of "method" is done.
+		private struct methodToRun
+		{
+			Action _method;//Method to be called on this methodQueue's owner thread.
+			public Action method { get { return _method; } }
+			Thread _source;//Thread that sent this request.
+			public Thread source { get { return _source; } }
+			Action _callback;//Method call to send back to _source after _method has been called.
+			public Action callback { get { return _callback; } }
+
+			public methodToRun(Action Method, Thread Source, Action Callback)
+			{
+				_method = Method;
+				_source = Source;
+				_callback = Callback;
+			}
+		}
+		//a Queue of methods to run.
+		private readonly Queue<methodToRun> _methodsToRun;
+		private Thread thread;//The thread owning this queue. methods can only be executed on this thread.
+
+		//Called when creating the MethodQueue, sets the thread and _methodToRun Queue.
+		public MethodQueue(Thread _thread)
+		{
+			thread = _thread;
+			_methodsToRun = new Queue<methodToRun>();
+		}
+
+		public void Tick()
+		{
+			if (Thread.CurrentThread == thread) //Checks to ensure that we can only be run on the Thread which owns this MethodQueue
+			{
+				lock (_methodsToRun) //Lock the methodQueue to ensure thread safety.
+				{
+					while (_methodsToRun.Count > 0) //Run through all queued methods.
+					{
+						methodToRun thisMethod = _methodsToRun.Dequeue(); //grab this method information.
+						thisMethod.method.Invoke(); //Run the method.
+						if (thisMethod.source != null && thisMethod.callback != null) //If information for a callback is present, trigger the setup so that it's added to the source thread's MethodQueue.
+							setupCallBack(thisMethod);
+					}
+				}
+			}
+		}
+
+		//Quick call to setup a the callback method on the _source Thread's methodQueue.
+		private void setupCallBack(methodToRun thisMethod)
+		{
+			Program.methodQueues[thisMethod.source].AddMethod(thisMethod.callback);
+		}
+
+		//Add a method with no callback information.
+		public void AddMethod(Action action)
+		{
+			AddMethod(action, null, null);
+		}
+
+		//Add a method with callback information.
+		public void AddMethod(Action action, Thread caller, Action callback)
+		{
+			lock (_methodsToRun) //Always lock the _methodsToRun Queue to ensure thread safety.
+			{
+				_methodsToRun.Enqueue(new methodToRun(action, caller, callback));
+			}
+		}
+	}
+}
